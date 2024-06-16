6807d248cd0f226671a37c958b8687538f792e1c
cjw9595 <jiaweichen9595@gmail.com>
2024-06-17 00:38:11 +0200

    a little update in html and css

--- WebUserCards/src/app/service/random-user.service.ts
+++ WebUserCards/src/app/service/random-user.service.ts
@@ -19,22 +19,8 @@ export class RandomUserService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  /*private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-*/
   handleError(error: HttpErrorResponse){
-    let errorMessage = 'Unknown error!';
+    let errorMessage = "Unknown error!";
     if(error.error instanceof ErrorEvent) {
       errorMessage = `Error: ${error.error.message}`;
     }
